5abce8fb575f6d8c6c327de791863c1a8de57624
lyes <lyes.makhloufi@outlook.fr>
2020-06-28 22:53:06 +0200

    commit project

---
+++ src/com/emailApp/Email.java
@@ -0,0 +1,15 @@
+package com.emailApp;
+
+public class Email {
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String departement;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	
+	
+
+}
---
+++ src/com/emailApp/EmailApp.java
@@ -0,0 +1,5 @@
+package com.emailApp;
+
+public class EmailApp {
+
+}
